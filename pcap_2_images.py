9f4f42bb26ea2ac0e6fc9886acce9db60b7a14a2
Agios <thundereme@gmail.com>
2022-06-27 20:40:59 -0600

    compact file

--- pcap_2_images.py
+++ pcap_2_images.py
@@ -20,19 +20,17 @@ class ListEmpty(Exception):
 
     """
 
-    def __init__(self,tipo_archivo, mensaje="El directorio no contiene archivos de tipo "):
+    def __init__(self,tipo_archivo, mensaje="No pcap files in this directory"):
         self.tipo_archivo = tipo_archivo
         self.mensaje = mensaje
         super().__init__(self.mensaje)
 
     def __str__(self):
-        return f'{self.mensaje}{self.tipo_archivo} en el directorio -> {os.getcwd()}'
+        return f'{self.mensaje}{self.tipo_archivo} -> {os.getcwd()}'
     
 
 
-if platform.system() == "Linux": #Comprobar el tipo de sistema que es.
-    """--------------------------------- Paso 1 ---------------------------------"""
-    #01.- Realiza una lista de archivos pcap dentro de todo el directorio, incluyendo subcarpetas
+if platform.system() == "Linux":
     
     path = os.getcwd()
     list_of_files = list()
@@ -45,40 +43,35 @@ if platform.system() == "Linux": #Comprobar el tipo de sistema que es.
     
     if pcap_list == []: raise ListEmpty("pcap")
     
-    #lista de todos archivos pcap dentro del directorio
     for pcap_file in pcap_list:
         csv_file = pcap_file[:-5] + ".csv"
         if os.path.isfile(csv_file):
             continue
-        """--------------------------------- Paso 2 ---------------------------------"""
-        #02.- Crear las imagenes a través de 3fex usando consola de comandos
         try:
             """
-            comando a usar sudo snort -c /etc/snort/snort.conf -r traza_original.pcap crea archibo tipo U para agregar a 3fex y agregar -p 1 para prioridades de tipo 1
-            sudo 3fex -r traza_original.pcap -f traza_original.csv -o 8 -noips -u /var/log/snort/snort.log.1625018634 -p 1
+            example command:
+                sudo 3fex -r traza_original.pcap -f traza_original.csv -o 8 -noips -u /var/log/snort/snort.log.1625018634 -p 1
             """
+            
             u_command = "sudo snort -c /etc/snort/snort.conf -r '" + pcap_file +"'"
-            print("\nu_command: ", u_command)
-            os.system(u_command) #Creando archivo U
+            os.system(u_command)
             list_of_u_files = os.listdir("/var/log/snort/")
-            antiguo_u_file = sorted(list_of_u_files)[-1] #Archivo más recientemente creado
+            antiguo_u_file = sorted(list_of_u_files)[-1] # most recent file created
             u_file = antiguo_u_file + ".x"
             name = pcap_file.split("/")
             name = name[-1][:-5]
-            directorio = pcap_file[:-5] #os.path.join(pcap_file[:-5],name)
-            print("\ndirectorio: ", directorio)
+            directorio = pcap_file[:-5] # os.path.join(pcap_file[:-5],name)
             mov_u_file = "sudo mv " + "/var/log/snort/" + antiguo_u_file + " " + directorio + "_" + u_file
-            print("\nMoviendo archivo u al directorio: ", mov_u_file)
             os.system(mov_u_file)
             
             command = "sudo 3fex -r '" + pcap_file + "' -f '" + csv_file + "' -o 1 -noips -u '" + directorio + "_" + u_file + "' -p 1"
-            #Ejemplo   sudo 3fex -r traza_original.pcap -f traza_original.csv -o 8 -noips -u /var/log/snort/snort.log.1625018634 -p 1
-            print("\nIniciando creación de imagenes para el archivo: ",pcap_file)
-            print("\ncommand: ",command)
+            """
+            Example command:
+                sudo 3fex -r traza_original.pcap -f traza_original.csv -o 8 -noips -u /var/log/snort/snort.log.1625018634 -p 1
+            """
             os.system(command)
         except:
-            print("Comando inválido: ", command)
-            print("Elimine las imagenes creadas e intente nuevamente")
+            print("Invalid command: ", command)
             continue
         
         imagenes = [i[:-4] for i in os.listdir(os.getcwd()) if i [-4:] == ".jpg"]
@@ -86,34 +79,26 @@ if platform.system() == "Linux": #Comprobar el tipo de sistema que es.
         
         if imagenes == []:
             pcap_fail.append(pcap_file)
-            print("No se pudo ejecutar el archivo: ", pcap_file)
+            print("Error loading pcap file: ", pcap_file)
             continue
-        """--------------------------------- Paso 3 ---------------------------------"""
-        #03.- Crear carpetas junto a los archivos pcap con el nombre de cada archivo pcap
         try:
             folder = "mkdir -p " + pcap_file[:-5]
-            print("Creando la carpeta: ", folder)
             os.system(folder)
         except:
-            print("Comando inválido: ", folder)
+            print("Invalid command: ", folder)
             continue
         
-        """--------------------------------- Paso 4 ---------------------------------"""
-        #04.- Se mueven las imagenes creadas a cada carpeta respectiva a cada archivo pcap.
         contador = 0
         print("csv: " ,csv_file)
         df = pd.read_csv(csv_file, index_col=0)
         values = df["label"]
         for i in imagenes:
             try:
-                # Crear dataset
                 mover_archivo_comando = "mv " + i + " " + os.path.join(pcap_file[:-5],name) + "-" + i[:-4] + "_" + str(values[contador]) + ".jpg"
                 contador += 1
-                print("\nMoviendo imagenes generadas al directorio: ", directorio)
-                print("archivo: ", mover_archivo_comando)
                 os.system(mover_archivo_comando)
             except:
-                print("Comando inválido: ", mover_archivo_comando)
+                print("Invalid command: ", mover_archivo_comando)
                 continue
 else:
-    print("Este sistema operativo no es Linux")
+    print("This S.O. is not Linux")
